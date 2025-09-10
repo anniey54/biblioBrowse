2cad14c1bb495af3d20a3371e708632f11892f65
anniey54 <annie.ye@torontomu.ca>
2025-09-10 16:22:22 -0400

    add link to navbar logo

--- bibliobrowse-app/src/components/Logo.jsx
+++ bibliobrowse-app/src/components/Logo.jsx
@@ -1,13 +1,14 @@
-import styles from './Logo.module.css'
+import styles from './Logo.module.css';
+import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBookBookmark } from '@fortawesome/free-solid-svg-icons';
 
 const Logo = ({fontColour = 'var(--primary)'}) => {
   return (
-    <div className={styles.logo} style={{color: fontColour}}>
+    <Link to={'/'} className={styles.logo} style={{color: fontColour}}>
         <FontAwesomeIcon icon={faBookBookmark} size='2xl' />
         <h3>BiblioBrowse</h3>
-    </div>
+    </Link>
   )
 }
 
