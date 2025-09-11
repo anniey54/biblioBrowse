b4bf80ddae1374ec501b08952ec9fa35d3e7ed28
anniey54 <annie.ye@torontomu.ca>
2025-09-10 21:59:46 -0400

    Add account menu button for mobile navbar

--- bibliobrowse-app/src/components/Navbar.jsx
+++ bibliobrowse-app/src/components/Navbar.jsx
@@ -10,10 +10,17 @@ import '@szhsin/react-menu/dist/transitions/zoom.css';
 
 const Navbar = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const [isAccountMenuOpen, setIsAccountMenuOpen] = useState(false);
   const user = {
     name: 'User Name Name Name User Name Name Name User Name Name Name User Name Name Name User Name Name Name',
     profileURL: 'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     isAdmin: false
+  }
+
+  /* reset both menus in mobile navbar to false */
+  const resetMobileMenus = () => {
+    setIsMobileMenuOpen(false); 
+    setIsAccountMenuOpen(false);
   }
 
   /* user profile menu styling  */
@@ -62,11 +69,29 @@ const Navbar = () => {
           </div>
       </nav>
       {isMobileMenuOpen &&
-        <div className={styles.mobileMenu}>
-          <Link to={'/'} onClick={() => {setIsMobileMenuOpen(false)}}>Home</Link>
-          <Link to={'/books'} onClick={() => {setIsMobileMenuOpen(false)}}>Books</Link>
-          <Link to={'/collections'} onClick={() => {setIsMobileMenuOpen(false)}}>Collections</Link>
-          <Link to={'/signin'} onClick={() => {setIsMobileMenuOpen(false)}}>Sign in</Link>
+        <div className={styles.mobileNav}>
+          <Link to={'/'} onClick={() => {resetMobileMenus()}}>Home</Link>
+          <Link to={'/books'} onClick={() => {resetMobileMenus()}}>Books</Link>
+          <Link to={'/collections'} onClick={() => {resetMobileMenus()}}>Collections</Link>
+
+          {/* signin or user profile and menu */}
+          {Object.keys(user).length > 0
+              ? <div className={styles.mobileAccountMenu}>
+                  <div className={styles.accountName} onClick={() => {setIsAccountMenuOpen(!isAccountMenuOpen)}}>
+                    <p>{user.name}</p>
+                    <FontAwesomeIcon icon={isAccountMenuOpen ? faChevronUp : faChevronDown} size="lg"/>
+                  </div>
+                  {isAccountMenuOpen &&
+                     <div className={styles.mobileNav}>
+                      <Link to={'/'} onClick={() => {resetMobileMenus()}}>Profile</Link>
+                      <Link to={'/'} onClick={() => {resetMobileMenus()}}>Create Collection</Link>
+                      { user.isAdmin && <Link to={'/'} onClick={() => {resetMobileMenus()}}>Create Book</Link> }
+                      <Link to={'/'} onClick={() => {resetMobileMenus()}}>Logout</Link>
+                    </div>
+                  }
+                </div>
+              : <Link to={'/signin'} onClick={() => {resetMobileMenus()}}>Sign in</Link>
+            }
         </div>
       }
     </>
