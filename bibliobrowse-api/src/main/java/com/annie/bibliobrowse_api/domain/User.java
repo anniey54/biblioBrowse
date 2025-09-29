9866e3727095499ec61df4e8d17a13d5b04b6b1e
anniey54 <annie.ye@torontomu.ca>
2025-09-28 21:46:27 -0400

    edit user columns to match database attribute constrants

--- bibliobrowse-api/src/main/java/com/annie/bibliobrowse_api/domain/User.java
+++ bibliobrowse-api/src/main/java/com/annie/bibliobrowse_api/domain/User.java
@@ -1,5 +1,7 @@
 package com.annie.bibliobrowse_api.domain;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -10,7 +12,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import com.fasterxml.jackson.annotation.JsonInclude;
 
 @Entity
 @Data
@@ -24,13 +25,20 @@ public class User {
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	@Column(name = "user_id", unique = true, updatable = false)
 	private Long id;
-	@Column(unique = true)
+
+	@Column(unique = true, nullable = false)
 	private String email;
+
+  @Column(nullable = false)
 	private String username;
+
+  @Column(nullable = false, length = 16)
   private String salt;
-  @Column(name = "password_hash")
+
+  @Column(name = "password_hash", columnDefinition = "TEXT", nullable = false)
 	private String password;
+
   @Column(name = "profile_image")
-	private String photoUrl;
-  
+	private String profileImage;
+
 }
